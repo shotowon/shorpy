c5330e7135db4852ec81ed5738005e274d3d269a
shotowon <sanzhar.maratov@proton.me>
2025-01-12 20:55:47 +0500

    make alias field of url model unique

--- shorpy/models/url.py
+++ shorpy/models/url.py
@@ -13,5 +13,5 @@ class URL(Base):
     __tablename__ = "url"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    alias: Mapped[str] = mapped_column(String(length=6), unique=True)
     url: Mapped[str] = mapped_column(String())
-    alias: Mapped[str] = mapped_column(String(length=6))
