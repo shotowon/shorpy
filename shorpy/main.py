4aef6194281a61de5f5f6038fde0a6826b66b951
shotowon <sanzhar.maratov@proton.me>
2025-01-13 06:47:28 +0500

    main function

---
+++ shorpy/main.py
@@ -0,0 +1,32 @@
+from contextlib import asynccontextmanager
+import logging
+import asyncio
+
+from sqlalchemy import text
+from fastapi import FastAPI
+import uvicorn
+
+from shorpy.core.app import cfg, db_gear
+from shorpy.api.router import router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_gear.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=router)
+
+
+def main():
+    logger = logging.getLogger("main")
+    logger.info(f"starting FastAPI server on port {cfg.http_server.port}")
+    uvicorn.run("shorpy.main:app", reload=True, port=cfg.http_server.port)
+
+
+if __name__ == "__main__":
+    main()
